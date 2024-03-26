45edbe7f855cc1bce26615f2feb8078fcf36a428
SEYEON-PARK <fourleaf123@naver.com>
2024-03-26 22:32:32 +0900

    five

--- src/a201514199/Main.java
+++ src/a201514199/Main.java
@@ -3,7 +3,9 @@ package a201514199;
 public class Main {
 
   public static void main(String[] args) {
+    Five.print();
     Two.print();
     System.out.println("one 201514199");
+    Three.print();
   }
 }
